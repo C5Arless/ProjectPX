8b97eb7fcab164064722579eb7a945d20250cd55
Paolo Castellino <36105321+C5Arless@users.noreply.github.com>
2025-07-29 12:31:12 +0200

    DoorAsyncEvent update

--- Assets/Scripts/AsyncEvents/DoorAsyncEvent.cs
+++ Assets/Scripts/AsyncEvents/DoorAsyncEvent.cs
@@ -1,12 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 
 public class DoorAsyncEvent : MonoBehaviour, IOrchestratedEvent {
+    [SerializeField] GameObject _doorL;
+    [SerializeField] GameObject _doorR;
+    [Space]
     [SerializeField] int eventIndex;
     [SerializeField] bool isRepeatable;
     [SerializeField] EventPriority priority;
+
+    private const float minScale = .5f;
+    private const float maxScale = 2.6f;
+    private const float speed = 3.15f;
 
     private bool isRunning;
 
@@ -35,16 +40,23 @@ public class DoorAsyncEvent : MonoBehaviour, IOrchestratedEvent {
     public async Task FireEvent() {
         isRunning = true;
 
-        await Task.Yield();
+        while (_doorL.transform.localScale.x <= maxScale) {
+            Vector3 currentScale = _doorL.transform.localScale;
+
+            currentScale.x += speed * Time.deltaTime;
+
+            _doorL.transform.localScale = currentScale;
+            _doorR.transform.localScale = currentScale;
+
+            await Task.Yield();
+        }
+
+        Vector3 finalScale = _doorL.transform.localScale;
+        finalScale.x = maxScale;
+
+        _doorL.transform.localScale = finalScale;
+        _doorR.transform.localScale = finalScale;
 
         isRunning = false;
-    }
-
-    void Start() {
-        
-    }
-    
-    void Update() {
-        
     }
 }
